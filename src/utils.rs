ec17b5e5d077f937e0a09f997132150a9ec21a13
Sofienne Lassoued <soflass1293@gmail.com>
2024-02-10 18:55:00 +0100

    Added vector sorting

---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+use std::iter;
+use rand::Rng;
+pub fn id(len: usize) -> String {
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let mut rng = rand::thread_rng();
+    let one_char = || CHARSET[rng.gen_range(0..CHARSET.len())] as char;
+    iter::repeat_with(one_char).take(len).collect()
+}
